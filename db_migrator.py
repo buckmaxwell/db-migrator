617f7677b4f148508b5cb09104312d4f13592ee9
buckmaxwell <buck.169@osu.edu>
2015-07-27 15:01:43 -0400

    debug

--- db_migrator.py
+++ db_migrator.py
@@ -2,7 +2,7 @@ import psycopg2
 import sqlite3
 
 pgconn = psycopg2.connect('postgresql://%s:%s@atena-db-dev.clfg28nzaugf.us-east-1.rds.amazonaws.com:5432/ais_srm_dev' % ('aetnaDeveloper', 'Safety2015Risk*'))
-slconn = sqlite3.connect('~/db-migrator/application_database.db')
+slconn = sqlite3.connect('application_database.db')
 
 pcur = pgconn.cursor()
 scur = slconn.cursor()
