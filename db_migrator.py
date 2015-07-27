f3aed74fcd87c7504740b75ec319c9f6b9c34c1c
buckmaxwell <buck.169@osu.edu>
2015-07-27 14:55:05 -0400

    shh

---
+++ db_migrator.py
@@ -0,0 +1,20 @@
+import psycopg2
+import sqlite3
+
+pgconn = psycopg2.connect('postgresql://%s:%s@atena-db-dev.clfg28nzaugf.us-east-1.rds.amazonaws.com:5432/ais_srm_dev' % ('aetnaDeveloper', 'Safety2015Risk*'))
+slconn = sqlite3.connect('~/application_database.db')
+
+pcur = pgconn.cursor()
+scur = slconn.cursor()
+
+
+#parts
+pcur.execute('SELECT * FROM parts')
+for row in pcur.fetchall():
+	print row
+#subarts
+#standards
+#violations
+#violations2
+#violations3
+#violations4
