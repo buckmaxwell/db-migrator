a71456b11d8b7dc114fb909416c425a425316741
buckmaxwell <buck.169@osu.edu>
2015-07-27 19:40:21 -0400

    finished  -- ready to attempt running

--- db_migrator.py
+++ db_migrator.py
@@ -12,7 +12,8 @@ print "BEGINNING MIGRATION"
 #parts
 scur.execute('SELECT number, title FROM parts')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_parts (number, title, update_number, active) VALUES (?, ?, ?, ?)' % (row[0], row[1], 1, True) )
+	pcur.execute('INSERT INTO dbv_parts (number, title, update_number, active) VALUES (%s, %s, %s, %s)', 
+		(row[0], row[1], 1, True) )
 	pgconn.commit()
 
 print "SUCCESSFULLY INSERTED PARTS"
@@ -20,7 +21,7 @@ print "SUCCESSFULLY INSERTED PARTS"
 #subarts
 scur.execute('SELECT letter, part_number, title FROM subarts')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_subparts (letter, part_number, title, update_number, active)' % 
+	pcur.execute('INSERT INTO dbv_subparts (letter, part_number, title, update_number, active) VALUES (%s, %s, %s, %s, %s)',
 		(row[0], row[1], row[2], 1, True))
 	pgconn.commit()
 
@@ -29,7 +30,7 @@ print "SUCCESSFULLY INSERTED SUBPARTS"
 #standards
 scur.execute('SELECT number,title,subpart_letter,part_number FROM standards')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_standards number, title, subpart_letter, part_number, update_number, active' %
+	pcur.execute('INSERT INTO dbv_standards number, title, subpart_letter, part_number, update_number, active VALUES (%s, %s, %s, %s, %s, %s)',
 		(row[0], row[1], row[2], row[3], 1, True))
 	pgconn.commit()
 
@@ -39,7 +40,7 @@ print "SUCCESSFULLY INSERTED STANDARDS"
 #violations
 scur.execute('SELECT number,description,standard_number FROM violations')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_violations number, description, standard_number, update_number, active' %
+	pcur.execute('INSERT INTO dbv_violations number, description, standard_number, update_number, active VALUES (%s, %s, %s, %s, %s)', 
 		(row[0], row[1], row[2], 1, True))
 	pgconn.commit()
 
@@ -49,7 +50,7 @@ print "SUCCESSFULLY INSERTED VIOLATIONS"
 #violations2
 scur.execute('SELECT number,description,violation_number FROM violations_level_2')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_violation_2s number, description, violation_number, update_number, active' %
+	pcur.execute('INSERT INTO dbv_violation_2s number, description, violation_number, update_number, active VALUES (%s, %s, %s, %s, %s)',
 		(row[0], row[1], row[2], 1, True))
 	pgconn.commit()
 
@@ -59,7 +60,7 @@ print "SUCCESSFULLY INSERTED VIOLATIONS2"
 #violations3
 scur.execute('SELECT number,description,violations_level_2_number FROM violations_level_3')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_violation_3s number, description, violation2_number, update_number, active' %
+	pcur.execute('INSERT INTO dbv_violation_3s number, description, violation2_number, update_number, active VALUES (%s, %s, %s, %s, %s)',
 		(row[0], row[1], row[2], 1, True))
 	pgconn.commit()
 
@@ -70,7 +71,7 @@ print "SUCCESSFULLY INSERTED VIOLATIONS3"
 #violations4
 scur.execute('SELECT number,description,violations_level_3_number FROM violations_level_3')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_violation_3s number, description, violation3_number, update_number, active' %
+	pcur.execute('INSERT INTO dbv_violation_3s number, description, violation3_number, update_number, active VALUES (%s, %s, %s, %s, %s)',
 		(row[0], row[1], row[2], 1, True))
 	pgconn.commit()
 
