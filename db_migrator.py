21ac9a9ce794cbf07de7ad09804725168ca95547
buckmaxwell <buck.169@osu.edu>
2015-07-27 20:42:25 -0400

    finished  -- ready to attempt running

--- db_migrator.py
+++ db_migrator.py
@@ -49,43 +49,74 @@ for row in scur.fetchall():
 
 print "SUCCESSFULLY INSERTED VIOLATIONS"
 
+
+def add_missing_violation(violation_number):
+	standard_number = violation_number.split('(')[0]
+	pcur.execute('INSERT INTO dbv_violations (number, description, standard_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+	(violation_number, violation_number, standard_number, 1, True))
+	pgconn.commit()
+	
+
 #violations2
 scur.execute('SELECT number,description,violation_number FROM violations_level_2')
 for row in scur.fetchall():
-	try:
-		pcur.execute('INSERT INTO dbv_violation_2s (number, description, violation_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
-			(row[0], row[1], row[2], 1, True))
-		pgconn.commit()
-	except psycopg2.IntegrityError as e:
-		pgconn.rollback()
-		print "ERROR1: " + str(e)
-		print type(e)
+	failure = True
+	while failure:
+		try:
+			pcur.execute('INSERT INTO dbv_violation_2s (number, description, violation_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+				(row[0], row[1], row[2], 1, True))
+			pgconn.commit()
+			failure = False
+		except psycopg2.IntegrityError as e:
+			pgconn.rollback()
+			add_missing_violation(row[2])
 
 print "SUCCESSFULLY INSERTED VIOLATIONS2"
+
+def add_missing_violation2(violation2_number):
+	violation_number = violation2_number.split('(')[0]
+	pcur.execute('INSERT INTO dbv_violation_2s (number, description, violation_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+	(violation2_number, violation2_number, violation_number, 1, True))
+	pgconn.commit()
 
 
 #violations3
 scur.execute('SELECT number,description,violations_level_2_number FROM violations_level_3')
 for row in scur.fetchall():
-	try:
-		pcur.execute('INSERT INTO dbv_violation_3s (number, description, violation2_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
-			(row[0], row[1], row[2], 1, True))
-		pgconn.commit()
-	except psycopg2.IntegrityError as e:
-		pgconn.rollback()
-		print "ERROR2: " + str(e)
-		print type(e)
-
+	failure = True
+	while failure:
+		try:
+			pcur.execute('INSERT INTO dbv_violation_3s (number, description, violation2_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+				(row[0], row[1], row[2], 1, True))
+			pgconn.commit()
+			failure = False
+		except psycopg2.IntegrityError as e:
+			pgconn.rollback()
+			add_missing_violation2(row[2])
+			#add violation 2
+	
 print "SUCCESSFULLY INSERTED VIOLATIONS3"
 
 
+def add_missing_violation3(violation3_number):
+	violation2_number = violation3_number.split('(')[0]
+	pcur.execute('INSERT INTO dbv_violation_3s (number, description, violation2_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+	(violation3_number, violation3_number, violation2_number, 1, True))
+	pgconn.commit()
 
 #violations4
 scur.execute('SELECT number,description,violations_level_3_number FROM violations_level_4')
 for row in scur.fetchall():
-	pcur.execute('INSERT INTO dbv_violation_4s (number, description, violation3_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
-		(row[0], row[1], row[2], 1, True))
-	pgconn.commit()
+	failure = True
+	while failure:
+		try:
+			pcur.execute('INSERT INTO dbv_violation_4s (number, description, violation3_number, update_number, active) VALUES (%s, %s, %s, %s, %s)',
+				(row[0], row[1], row[2], 1, True))
+			pgconn.commit()
+			failure = False
+		except psycopg2.IntegrityError as e:
+			pgconn.rollback()
+			add_missing_violation3(row[2])
 
 print "SUCCESSFULLY INSERTED VIOLATIONS4"
 
